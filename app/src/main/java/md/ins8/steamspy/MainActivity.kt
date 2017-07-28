b87a079fc065f5642ee479baf3bb40578cb9cc46
Alexander Bondarenco <alexandr.bondarenco@endava.com>
2017-07-28 08:18:31 +0300

    Add a toolbar

--- app/src/main/java/md/ins8/steamspy/MainActivity.kt
+++ app/src/main/java/md/ins8/steamspy/MainActivity.kt
@@ -2,11 +2,14 @@ package md.ins8.steamspy
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
 import co.zsmb.materialdrawerkt.builders.drawer
 import co.zsmb.materialdrawerkt.draweritems.badgeable.primaryItem
 import co.zsmb.materialdrawerkt.draweritems.badgeable.secondaryItem
 import co.zsmb.materialdrawerkt.draweritems.divider
 import co.zsmb.materialdrawerkt.draweritems.expandable.expandableItem
+import com.afollestad.materialcab.MaterialCab
 
 class MainActivity : AppCompatActivity() {
 
@@ -36,8 +39,26 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+
+        val materialCab = MaterialCab(this, R.id.cab_stub).start(object : MaterialCab.Callback {
+            override fun onCabFinished(cab: MaterialCab?): Boolean {
+                return false
+            }
+
+            override fun onCabItemClicked(item: MenuItem?): Boolean {
+                return false
+            }
+
+            override fun onCabCreated(cab: MaterialCab?, menu: Menu?): Boolean {
+                cab?.setTitle("Home: Watchlist")
+                return true
+            }
+        })
+
+
         drawer {
             headerViewRes = R.layout.drawer_header
+            toolbar = materialCab.toolbar
             primaryItem(R.string.navigation_home) {
                 onClick { _ -> onHome(); false }
             }
@@ -74,5 +95,7 @@ class MainActivity : AppCompatActivity() {
             primaryItem(R.string.navigation_settings) { }
             primaryItem(R.string.navigation_about) { }
         }
+
+
     }
 }
