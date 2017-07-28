01152f6de36c65fa38eb9e38f73165a8bd8fceb9
Alexander Bondarenco <alexandr.bondarenco@endava.com>
2017-07-28 08:01:23 +0300

    Improve navigation drawer

--- app/src/main/java/md/ins8/steamspy/MainActivity.kt
+++ app/src/main/java/md/ins8/steamspy/MainActivity.kt
@@ -2,26 +2,77 @@ package md.ins8.steamspy
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.widget.AdapterView
-import android.widget.ArrayAdapter
-import android.widget.Toast
-import kotlinx.android.synthetic.main.activity_main.*
+import co.zsmb.materialdrawerkt.builders.drawer
+import co.zsmb.materialdrawerkt.draweritems.badgeable.primaryItem
+import co.zsmb.materialdrawerkt.draweritems.badgeable.secondaryItem
+import co.zsmb.materialdrawerkt.draweritems.divider
+import co.zsmb.materialdrawerkt.draweritems.expandable.expandableItem
 
 class MainActivity : AppCompatActivity() {
-    private val navigationOptions = listOf(
-            "All", "Top", "Genre", "Watchlist"
-    )
+
+    private fun onHome() {
+
+    }
+
+    private fun onAll() {
+
+    }
+
+    private fun onTop2Weeks() {
+
+    }
+
+    private fun onOwned() {}
+
+    private fun onTotal() {}
+
+    private fun onNotifications() {}
+
+    private fun onSettings() {}
+
+    private fun onAbout() {}
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        navigationList.adapter = ArrayAdapter<String>(this, R.layout.drawer_list_item, navigationOptions)
-        navigationLayout.setStatusBarBackgroundColor(android.R.color.holo_red_dark)
-
-        navigationList.onItemClickListener = AdapterView.OnItemClickListener {
-            p0, p1, p2, p3 ->
-            Toast.makeText(this, "Item clicked", Toast.LENGTH_SHORT).show()
+        drawer {
+            headerViewRes = R.layout.drawer_header
+            primaryItem(R.string.navigation_home) {
+                onClick { _ -> onHome(); false }
+            }
+            primaryItem(R.string.navigation_all) {
+                onClick { _ -> onAll(); false }
+            }
+            divider { }
+            expandableItem(R.string.navigation_top) {
+                secondaryItem("2 Weeks") {
+                    onClick { _ -> onTop2Weeks(); false }
+                }
+                secondaryItem("Owned") { }
+                secondaryItem("Total") { }
+                selectable = false
+            }
+            divider { }
+            expandableItem(R.string.navigation_genre) {
+                secondaryItem("Action") { }
+                secondaryItem("Strategy") { }
+                secondaryItem("RPG") { }
+                secondaryItem("Indie") { }
+                secondaryItem("Adventure") { }
+                secondaryItem("Sports") { }
+                secondaryItem("Simulation") { }
+                secondaryItem("Early Access") { }
+                secondaryItem("Ex Early Access") { }
+                secondaryItem("MMO") { }
+                secondaryItem("Free") { }
+                secondaryItem("RPG") { }
+                selectable = false
+            }
+            divider { }
+            primaryItem(R.string.navigation_notifications) { }
+            primaryItem(R.string.navigation_settings) { }
+            primaryItem(R.string.navigation_about) { }
         }
     }
 }
