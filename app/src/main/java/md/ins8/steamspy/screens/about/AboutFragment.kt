10873720c634080126951617b05c162adec55d47
Alexander Bondarenco <alexander.bondarenco@endava.com>
2017-09-01 22:12:52 +0300

    Create about screen

--- app/src/main/java/md/ins8/steamspy/screens/about/AboutFragment.kt
+++ app/src/main/java/md/ins8/steamspy/screens/about/AboutFragment.kt
@@ -5,13 +5,18 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import kotlinx.android.synthetic.main.fragment_about.*
 import md.ins8.steamspy.R
 
 
 class AboutFragment : Fragment() {
-    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val view = inflater?.inflate(R.layout.fragment_about, container, false)
+    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? =
+            inflater?.inflate(R.layout.fragment_about, container, false)
 
-        return view
+    override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val pInfo = context.packageManager.getPackageInfo(context.packageName, 0)
+        val versionName = pInfo.versionName
+        version.text = versionName
     }
 }
