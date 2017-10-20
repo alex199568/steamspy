2bddd4264bdd44d992806f4f4a742aa9a9c87221
Alexander Bondarenco <alexandru.bondarenco@travelportdigital.com>
2017-10-20 16:12:16 +0300

    Don't update data if data is updating

--- app/src/main/java/md/ins8/steamspy/service/update/DataUpdateService.kt
+++ app/src/main/java/md/ins8/steamspy/service/update/DataUpdateService.kt
@@ -20,6 +20,8 @@ private const val INTENT_SERVICE_NAME = "DataUpdateIntentService"
 const val LAST_UPDATE_TIME_KEY = "LastUpdateTimeKey"
 const val TO_CHECK_PARAM_EXTRA = "ToCheckParamExtra"
 
+private var isServiceRunning = false
+
 class DataUpdateService : IntentService(INTENT_SERVICE_NAME) {
     @Inject
     lateinit var steamAppsAPIService: SteamSpyAPIService
@@ -29,7 +31,6 @@ class DataUpdateService : IntentService(INTENT_SERVICE_NAME) {
     private lateinit var notificationManager: NotificationManager
     private lateinit var notificationBuilder: NotificationCompat.Builder
 
-    private var toCheck = false
 
     override fun onCreate() {
         super.onCreate()
@@ -38,16 +39,20 @@ class DataUpdateService : IntentService(INTENT_SERVICE_NAME) {
         setupNotifications()
     }
 
-    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
-        if (intent != null) {
-            toCheck = intent.getBooleanExtra(TO_CHECK_PARAM_EXTRA, false)
-        }
-        return super.onStartCommand(intent, flags, startId)
-    }
-
     override fun onHandleIntent(workIntent: Intent?) {
-        if (!toCheck || autoUpdateChecker.check()) {
+        if (!isServiceRunning) {
+            isServiceRunning = true
+            var toCheck = false
+            if (workIntent != null) {
+                toCheck = workIntent.getBooleanExtra(TO_CHECK_PARAM_EXTRA, false)
+            }
+            if (toCheck) {
+                if (autoUpdateChecker.check()) {
+                    doUpdate()
+                }
+            }
             doUpdate()
+            isServiceRunning = false
         }
     }
 
