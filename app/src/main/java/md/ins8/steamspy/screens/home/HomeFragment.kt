be532daf89fa3ff766f8f191d8245ad0cbc8d6a1
Alexander Bondarenco <alexandru.bondarenco@travelportdigital.com>
2017-10-21 23:11:16 +0300

    Fix OutOfMemoryError
    Add Russian translation for auto update preference section

--- app/src/main/java/md/ins8/steamspy/screens/home/HomeFragment.kt
+++ app/src/main/java/md/ins8/steamspy/screens/home/HomeFragment.kt
@@ -5,19 +5,12 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import kotlinx.android.synthetic.main.fragment_home.*
 import md.ins8.steamspy.R
-import md.ins8.steamspy.retrieveVersionName
 
 class HomeFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater?.inflate(R.layout.fragment_home, container, false)
 
         return view
-    }
-
-    override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        homeVersion.text = retrieveVersionName(context)
     }
 }
