c8485bbd986c3b242838ab21049fa6c1cf8bd5eb
Alexander Bondarenco <alexandru.bondarenco@travelportdigital.com>
2018-04-05 22:44:23 +0300

    Replace launcher icons

--- app/src/main/java/md/ins8/steamspy/service/update/DataUpdateService.kt
+++ app/src/main/java/md/ins8/steamspy/service/update/DataUpdateService.kt
@@ -138,7 +138,7 @@ class DataUpdateService : IntentService(INTENT_SERVICE_NAME) {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             notificationBuilder.setSmallIcon(R.drawable.spyglass_white)
         } else {
-            notificationBuilder.setSmallIcon(R.mipmap.ic_launcher_square)
+            notificationBuilder.setSmallIcon(R.mipmap.ic_launcher)
         }
     }
 
